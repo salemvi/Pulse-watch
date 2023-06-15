11fc337679af14c4c2b5c9b29b663b186153c345
Vincent <zamanovvr@gmail.com>
2023-06-15 11:51:17 +0300

    pathTwo
    slider navigation and animation for this

---
+++ src/js/script.js
@@ -0,0 +1,57 @@
+
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  responsive: {
+    640: {
+      edgePadding: 20,
+      gutter: 20,
+      items: 1,
+      nav: true
+    },
+    1200: {
+      nav: false
+    }
+  }
+
+});
+document.querySelector('.prev').addEventListener('click', function ()  {
+    slider.goTo('prev');
+  });
+  document.querySelector('.next').addEventListener('click', function ()  {
+    slider.goTo('next');
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         // adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></img></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></img></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                     dots: true,
+//                     arrows: false
+//                 }
+//             }    
+//         ]
+//     });
+
+// });
